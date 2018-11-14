cb300cc5b4a2f976d9f10e69efe41834c9bf2a03
lixiaode222 <1803118517@qq.com>
2018-11-14 12:13:44 +0000

    管理后台 - 商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,6 @@ Route::group([
     $router->get('/', 'HomeController@index');
     //后台用户列表页面
     $router->get('users', 'UsersController@index');
+    //后台商品列表页面
+    $router->get('products','ProductsController@index');
 });
