c35adeba82abc18738943cc053d29d0008ade8e1
lixiaode222 <1803118517@qq.com>
2018-11-16 01:35:54 +0000

    完善支付后逻辑

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use App\Listeners\RegisteredListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
+use App\Events\OrderPaid;
+use App\Listeners\UpdateProductSoldCount;
+use App\Listeners\SendOrderPaidMail;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -15,8 +18,17 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
+        //注册后发送邮箱验证
         Registered::class => [
             RegisteredListener::class,
+        ],
+
+        //支付后触发的事件
+        OrderPaid::class => [
+            //更新商品销量
+            UpdateProductSoldCount::class,
+            //发送邮件提醒已经完成支付
+            SendOrderPaidMail::class,
         ],
     ];
 
